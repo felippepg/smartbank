13108df3c97cba017d38beb363e2373238ba64fb
Felippe Pires <felippepg.goncalves@outlook.com.br>
2022-07-28 09:53:02 -0300

    Refatorando estilização com styled components

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import styled from "styled-components";
 
+const Header = styled.nav`
+  background-color: #41d3be;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+  cursor: pointer;
+`;
+
+const MainBtn = styled.a`
+ text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  color: ${(props) => props.primary ? "white" : "#41d3be"};
+  background: ${(props) => props.primary ? "transparent" : "white"};
+
+`;
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
-      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
+    <Header>
+      <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <MainBtn href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </MainBtn>
+        <MainBtn primary href="https://google.com">
           Sair
-        </a>
+        </MainBtn>
       </div>
-    </div>
+    </Header>
   );
 };
 
